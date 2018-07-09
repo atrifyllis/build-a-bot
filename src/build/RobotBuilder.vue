d27cea09c40f313b9ce45590325fd4be98f57455
Alexandros Trifyllis <a.trifyllis@gmail.com>
2018-07-09 14:42:08 +0300

    add class binding

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,7 +2,7 @@
     <div class="content">
         <button class="add-to-cart" @click="addToCart()"> Add to Cart</button>
         <div class="top-row">
-            <div class="top part">
+            <div :class="[saleBorderClass, 'top', 'part']">
                 <div class="robot-name">
                     {{selectedRobot.head.title}}
                     <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
@@ -92,6 +92,16 @@ export default {
         torso: availableParts.torsos[this.selectedTorsoIndex],
         rightArm: availableParts.arms[this.selectedRightArmIndex],
         base: availableParts.bases[this.selectedBaseIndex],
+      };
+    },
+    saleBorderClass() {
+      return this.selectedRobot.head.onSale ? 'sale-border' : '';
+    },
+    headBorderStyle() {
+      return {
+        border: this.selectedRobot.head.onSale ?
+          '3px solid red' :
+          '3px solid #aaa',
       };
     },
   },
@@ -305,5 +315,9 @@ td, th {
 
 .cost {
     text-align: right;
+}
+
+.sale-border {
+    border: 3px solid red;
 }
 </style>
